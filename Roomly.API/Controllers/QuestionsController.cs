0aa53203e33dd40281af2be84edc83b6c17eec6f
luke-truitt <38018183+luke-truitt@users.noreply.github.com>
2018-10-13 19:04:03 -0400

    API

---
+++ Roomly.API/Controllers/QuestionsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Roomly.API.Contracts;
+using Roomly.Business;
+
+namespace Roomly.API.Controllers
+{
+    [Produces("application/json")]
+    public class QuestionsController : Controller
+    {
+        private readonly IQuestionService _questionService;
+
+        public QuestionsController(IQuestionService questionService)
+        {
+            _questionService = questionService;
+        }
+
+        
+        [HttpGet("/questions/")]
+        public IActionResult Get()
+        {
+            var models = _questionService.Get();
+            return Ok(models);
+        }
+
+        
+        [HttpGet("/questions/{questionId}")]
+        public IActionResult Get(Guid questionId)
+        {
+            var model = _questionService.Get(questionId);
+            return Ok(model);
+        }
+
+       
+        [HttpPost("/questions/")]
+        public Question Post([FromBody] Question model)
+        {
+            var entity = _questionService.Get(model.QuestionId);
+            var result = new Question();
+            if (entity == null)
+            {
+                result = _questionService.Create(model);
+                return result;
+            }
+
+            result = _questionService.Update(model);
+            return result;
+
+        }
+
+     
+        [HttpPut("/questions/{questionId}")]
+        public Question Put(Guid questionId, [FromBody] Question model)
+        {
+            return Post(model);
+            
+        }
+
+
+        [HttpDelete("/questions/{questionId}")]
+        public void Delete(Guid questionId)
+        {
+            var model = _questionService.Get(questionId);
+            model.IsDeleted = true;
+            model.IsActive = false;
+            _questionService.Update(model);
+        }
+    }
+}
