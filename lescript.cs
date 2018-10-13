7f3af9321c5445d76cad733451afef9b7842496f
nosyarg <york17a@ncssm.edu>
2018-10-13 14:48:25 -0400

    the rest o my shit

--- lescript.cs
+++ lescript.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class lescript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        /*
+                Question q1 = new Question("First Question", 0, "1+1=", false, true, false , "1", "2", "3", "4", "5", "2");
+                Question q2 = new Question("second", 0, "Grayson is Awesome T/F?", false, false, true, null, null, null, null, null, "TRUE");
+                Question q3 = new Question("third", 1, "how many roads must a man walk", true, false, false, null, null, null, null, null, "42");
+                print(q3.Body);
+                */
+
 	}
 	
 	// Update is called once per frame
@@ -14,3 +21,35 @@ public class lescript : MonoBehaviour {
 		
 	}
 }
+/*
+public class Question
+{
+        public Question(string title, int category, string body, bool shortAnswer, bool multipleChoice, bool trueFalse, string a, string b, string c, string d, string e, string ans)
+        {
+        Title = title;
+        Category = category;
+        Body = body;
+        ShortAnswer = shortAnswer;
+        MultipleChoice = multipleChoice; 
+        TrueFalse = trueFalse;
+        A = a;
+        B = b;
+        C = c;
+        D = d;
+        E = e;
+        Ans = ans;
+        }
+        public string Title {get; }
+        public int Category {get; }
+        public string Body {get; }
+        public bool ShortAnswer {get; }
+        public bool MultipleChoice {get; }
+        public bool TrueFalse {get; }
+        public string A {get; }
+        public string B {get; }
+        public string C {get; }
+        public string D {get; }
+        public string E {get; }
+        public string Ans {get; }
+}
+*/
