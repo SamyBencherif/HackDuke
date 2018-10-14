f1223647cd53809c1a1cdbdb57af286f69d3c3fa
Samy Bencherif <samybencherif@live.com>
2018-10-13 20:58:35 -0400

    fixed the god damn mf doors

---
+++ showHint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showHint : MonoBehaviour
+{
+
+	public GameObject hint;
+	GlobalVars vars;
+
+	void OnTriggerEnter(Collider collision)
+	{
+		if (vars.showDoorHint)
+		{
+			hint.SetActive (true);
+			vars.showDoorHint = false;
+		}
+	}
+
+	void OnTriggerExit(Collider collision)
+	{
+		hint.SetActive (false);
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		vars = GameObject.Find ("GlobalVars").GetComponent<GlobalVars> ();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+}
